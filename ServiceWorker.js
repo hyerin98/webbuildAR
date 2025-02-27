157ccb43e91a7c8e1250fa362f885001f60c46f7
hyerinleee <hyerin.lee@im-fine.co.kr>
2025-02-27 15:29:11 +0900

    no message

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.0";
+const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.1";
 const contentToCache = [
     "Build/RRR.loader.js",
     "Build/RRR.framework.js.unityweb",
